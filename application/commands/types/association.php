dd46f50564de64f44fdb76a6776ef00ec9821824
Konstantinos Kouratoras <konstantinos.k@onthegosystems.com>
2018-01-19 15:12:33 +0200

    Add parameter to set first item in association

--- application/commands/types/association.php
+++ application/commands/types/association.php
@@ -77,15 +77,20 @@ class Association extends Types_Command {
 	 * : How many items of the second part involved in the relationship to generate for each one of the first part.
 	 * Default: 10
 	 *
+	 * [--post=<number>]
+	 * : The ID of the first post of the association. If used, count-first parameter should be ommitted. If ommitted, a
+	 * new post will be created.
+	 *
 	 * [--relationship=<string>]
 	 * : The relationship slug.
 	 *
 	 * ## EXAMPLES
 	 *
 	 *    wp types association generate --count-first=2 --count-second=20 --relationship=relationship-slug
+	 *    wp types association generate --post=12 --count-second=40 --relationship=relationship-slug
 	 *
 	 * @subcommand generate
-	 * @synopsis [--count-first=<number>] [--count-second=<number>] [--relationship=<string>]
+	 * @synopsis [--count-first=<number>] [--count-second=<number>] [--post=<number>] [--relationship=<string>]
 	 *
 	 * @since 1.0
 	 */
@@ -93,9 +98,14 @@ class Association extends Types_Command {
 		$defaults = array(
 			'count-first' => 1,
 			'count-second' => 10,
+			'post' => null,
 			'relationship' => null,
 		);
 		$assoc_args = wp_parse_args( $assoc_args, $defaults );
+
+		if ( ! is_null( $assoc_args['post'] ) && $assoc_args['count-first'] != 1 ) {
+			\WP_CLI::error( __( 'First count parameter cannot be set when post parameter is set.', 'toolset-cli' ) );
+		}
 
 		$definition_repository = \Toolset_Relationship_Definition_Repository::get_instance();
 		$definition = $definition_repository->get_definition( $assoc_args['relationship'] );
@@ -114,11 +124,16 @@ class Association extends Types_Command {
 
 		$progress = \WP_CLI\Utils\make_progress_bar( __( 'Generating associations', 'toolset-cli' ), $assoc_args['count-first'] * $assoc_args['count-second'] );
 		for ( $i = 0; $i < $assoc_args['count-first']; $i ++ ) {
-			$first_item = \WP_CLI::runcommand( 'post create --porcelain --post_status=publish --post_type=' . $first_post_type . ' --post_title=' . \Toolset_CLI\get_random_string(), $wpcli_command_options );
+
+			$first_item = $assoc_args['post'];
+
+			if ( is_null( $first_item ) ) {
+				$first_item = \WP_CLI::runcommand( 'post create --porcelain --post_status=publish --post_type=' . $first_post_type . ' --post_title=' . \Toolset_CLI\get_random_string(), $wpcli_command_options );
+			}
 
 			for ( $j = 0; $j < $assoc_args['count-second']; $j ++ ) {
 				$second_item = \WP_CLI::runcommand( 'post create --porcelain --post_status=publish --post_type=' . $second_post_type . ' --post_title=' . \Toolset_CLI\get_random_string(), $wpcli_command_options );
-				
+
 				try {
 					$assocation = $definition->create_association( $first_item, $second_item );
 
