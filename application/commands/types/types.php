5958fe23f135921c2beee186e0fc6c87da041920
Konstantinos Kouratoras <konstantinos.k@onthegosystems.com>
2018-06-08 17:12:04 +0300

    Add Types export command

--- application/commands/types/types.php
+++ application/commands/types/types.php
@@ -44,4 +44,41 @@ class Types extends Types_Command {
 			\WP_CLI::error( __( 'There was an error while importing the file.', 'toolset-cli' ) );
 		}
 	}
+
+	/**
+	 * Exports a Types file.
+	 *
+	 * ## Options
+	 *
+	 * <file>
+	 * : The filename of the exported file.
+	 *
+	 * ## Examples
+	 *
+	 *     wp types export file
+	 *
+	 * @subcommand export
+	 * @synopsis <file>
+	 *
+	 * @param array $args The arguments of the post type query.
+	 * @param array $assoc_args The associative arguments of the post type query.
+	 */
+	public function export( $args, $assoc_args ) {
+		list( $file ) = $args;
+
+		if ( empty ( $file ) ) {
+			\WP_CLI::warning( __( 'You must specify a valid file.', 'toolset-cli' ) );
+
+			return;
+		}
+
+		require_once WPCF_INC_ABSPATH . '/import-export.php';
+		$exported_data = wpcf_admin_export_data( false );
+
+		if ( file_put_contents($file, $exported_data) ) {
+			\WP_CLI::success( __( 'The file was exported successfully.', 'toolset-cli' ) );
+		} else {
+			\WP_CLI::error( __( 'There was an error while exporting the file.', 'toolset-cli' ) );
+		}
+	}
 }
