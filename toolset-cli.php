a67d462c23b52f4912968928061f164363e0e57f
Saul Baizman <saul@baizmandesign.com>
2021-03-28 18:54:36 -0400

    [#16] Upgrade the plugin directly from GitHub releases using the WordPress Upgrade mechanism.

--- toolset-cli.php
+++ toolset-cli.php
@@ -1,16 +1,19 @@
 <?php
 /*
 Plugin Name: Toolset CLI
-Description: WP-CLI commands for Toolset plugins
+Plugin URI: https://github.com/OnTheGoSystems/toolset-cli
+Description: WP-CLI commands for Toolset plugins.
 Author: OnTheGoSystems
-Author URI: http://www.onthegosystems.com
+Author URI: https://onthegosystems.com
 Version: 1.0.3
 License: GPLv2 or later
 */
 
-if ( defined( 'WP_CLI' ) && WP_CLI ) {
-	require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
+$bootstrap = new \OTGS\Toolset\CLI\Bootstrap();
 
-	$bootstrap = new \OTGS\Toolset\CLI\Bootstrap();
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	$bootstrap->initialize();
 }
+
+$bootstrap->initialize_updater(__FILE__);
